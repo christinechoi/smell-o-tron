75b036267685f0e3db88fd45559256a55ccaa511
christinechoi <smi77y@gmail.com>
2017-10-31 14:19:49 -0700

    Install redux and create store in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import perfumesReducer from './reducers/perfumesReducer';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(perfumesReducer);
+
+
+ReactDOM.render(
+  <App store={createStore} />, 
+  document.getElementById('root')
+);
+
+
