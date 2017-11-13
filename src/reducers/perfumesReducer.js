5d735bcf75aa00c4de819fd8d6a27644501815d5
christinechoi <smi77y@gmail.com>
2017-11-12 18:11:20 -0800

    Create initialState, fix add_perfume, and add get_recommendation

--- src/reducers/perfumesReducer.js
+++ src/reducers/perfumesReducer.js
@@ -1,14 +1,31 @@
 
-export default (state = [], action) => {
+const initialState = {
+  perfumes: [],
+  img: [],
+  name: [],
+  brand: []
+}
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_PERFUME':
-      // {debugger};
-      return action.payload;
-    case 'ADD_PERFUME':
       {debugger};
-
-      return { selectedPerfumes: action.payload };
+      return { ...state, perfumes: action.payload}
+    case 'ADD_PERFUME':
+      // {debugger};
+      const item = action.payload;
+      {debugger};
+      return {  
+        ...state, 
+        img: state.img.concat(item.src),
+        name: state.name.concat(item.name), 
+        brand: state.brand.concat(item.brand) 
+      }
+    case 'GET_RECOMMENDATION':
+      {debugger};
+      return { ...state, selectedPerfumes: action.payload };  
     default:
+      {debugger};
       return state;
   }   
 };
