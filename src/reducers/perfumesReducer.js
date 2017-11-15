0d9d43069ca3a66d9228a7326b6e57228baa62b7
christinechoi <smi77y@gmail.com>
2017-11-14 22:42:54 -0500

    Fix add_perfume action to include name and brand; add delete_perfume action

--- src/reducers/perfumesReducer.js
+++ src/reducers/perfumesReducer.js
@@ -9,23 +9,49 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_PERFUME':
-      {debugger};
+      // {debugger};
       return { ...state, perfumes: action.payload}
     case 'ADD_PERFUME':
-      // {debugger};
-      const item = action.payload;
+     
       {debugger};
       return {  
         ...state, 
-        img: state.img.concat(item.src),
-        name: state.name.concat(item.name), 
-        brand: state.brand.concat(item.brand) 
+        img: state.img.concat(action.payload.src),
+        name: state.name.concat(action.childNodes.children[0].innerText), 
+        brand: state.brand.concat(action.childNodes.children[1].innerText) 
       }
+
+    case 'DELETE_PERFUME':
+      let ind = state.name.indexOf(action.payload.name); 
+
+      let ary = state.img
+
+      let ex = ary.filter(function (x) {
+        let idx = state.img.indexOf(x);
+        return idx != ind
+      });
+
+      let modName = state.name.filter(function(x) {
+        let idx = state.name.indexOf(x);
+        return idx != ind 
+      });
+
+      let modBrand = state.brand.filter(function(x) {
+        let idx = state.brand.indexOf(x);
+        return idx != ind
+      });
+
+      return { 
+        ...state, 
+        img: ex,
+        name: modName, //state.name.slice(ind, ind+1),
+        brand: modBrand // state.brand.slice(ind, ind+1)
+      };  
     case 'GET_RECOMMENDATION':
       {debugger};
       return { ...state, selectedPerfumes: action.payload };  
     default:
-      {debugger};
+      // {debugger};
       return state;
   }   
 };
