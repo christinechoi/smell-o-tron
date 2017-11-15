1eb2b885706918063ce0f8762d81e0a4c4a3c9ae
christinechoi <smi77y@gmail.com>
2017-11-14 22:46:38 -0500

    Add delete_perfume to mapDispatchToProps; brand to mapStateToProps; removeOnClick handler; conditional rendering for search form; and added Semantic UI Form component

--- src/containers/PerfumesListContainer.js
+++ src/containers/PerfumesListContainer.js
@@ -1,9 +1,10 @@
 
 import React, { Component } from 'react';
+import { Form } from 'semantic-ui-react';
 import SearchResults from '../components/SearchResults';
 import SelectedPerfumes from '../components/SelectedPerfumes';
 
-import { fetchPerfume, addPerfume, getRecommendation } from '../actions/perfumesActions';
+import { fetchPerfume, addPerfume, deletePerfume, getRecommendation } from '../actions/perfumesActions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
@@ -20,10 +21,16 @@ class PerfumesListContainer extends Component {
     };
   }
 
+  removeOnClick = event => {
+    event.preventDefault();
+    {debugger};
+    this.props.deletePerfume(event.target);
+  }
+
   handleOnClick = event => {
     event.preventDefault();
     {debugger};
-    this.props.addPerfume(event.target);
+    this.props.addPerfume(event.target, event.currentTarget.childNodes[1]);
   }
 
   handleChange = event => {
@@ -42,36 +49,30 @@ class PerfumesListContainer extends Component {
   }
 
   render() {
-    // const perfumesExist = !!this.state.perfumes;
-    {debugger};
+    
     return(
 
       <div>
 
-        {(this.props.name.length === 0) ? null :
+        { (this.props.name.length === 0) ? null :
           <SelectedPerfumes
             img={this.props.img}
             name={this.props.name}
-            
-            handleOnClick={this.handleOnClick.bind(this)} 
+            brand={this.props.brand}
+            removeOnClick={this.removeOnClick} 
             /> 
         }
         
-        <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <h3> Search for a Perfume: </h3>
-          <p>
-            <input
-              type="text"
-              name="perfumes"
+        { (this.props.name.length === 3) ? null : 
+          <Form onSubmit={(event) => this.handleOnSubmit(event)}>
+            <Form.Input 
               value={this.state.value}
+              label="Search for a Perfume"
               onChange ={this.handleChange}
-              />
-              {this.state.text}
-          </p>
-          {this.state.value}
-          <input type="submit" />
-        </form>
-
+              type="text" />
+            <Form.Button>Submit</Form.Button>
+          </Form>
+        }
 
         {(this.props.perfumes.length === 0) ? null :
           <SearchResults 
@@ -85,29 +86,25 @@ class PerfumesListContainer extends Component {
     );
   }
 }
-// export default PerfumesListContainer;
-
 
 const mapStateToProps = (state) => { 
   // {debugger};
   return { 
     perfumes: state.perfumes.perfumes,
     img: state.perfumes.img,
-    name: state.perfumes.name
-    
+    name: state.perfumes.name,
+    brand: state.perfumes.brand
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
     fetchPerfume: fetchPerfume,
-    addPerfume: addPerfume
+    addPerfume: addPerfume,
+    deletePerfume: deletePerfume
   }, dispatch);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PerfumesListContainer);
-
-  
-
 
 
