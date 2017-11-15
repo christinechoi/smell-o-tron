6f1218f3e040db39994c6e00ef2a90660a4bf57e
christinechoi <smi77y@gmail.com>
2017-11-14 22:41:19 -0500

    Add semantic UI Cards to SelectedPerfumes component

--- src/components/SelectedPerfumes.js
+++ src/components/SelectedPerfumes.js
@@ -1,35 +1,97 @@
 import React from 'react';
-  
+import { Card, Button, Image } from 'semantic-ui-react';
+
 const SelectedPerfumes = (props) => {
-//   // {debugger};
-return (
-    <div className='search SearchResults'> {
-      props.perfumes.map((perfume, index) => (
-        <div key={index} >
-            <a href="#"  ><img 
-              onClick={props.handleOnClick}
-              className="image" 
-              src={perfume.pictureURL}
-              id={perfume.id}
-              name={perfume.name}
-              brand={perfume.brand} />
-            </a>
-            
-            <h4>{perfume.name} by {perfume.brand}</h4>
-        </div>
-      ))}
-    </div>
-  );
 
-
+  {debugger};
   return (
-    <div>
-      {props.img[0]}
-      {props.name[0]}
-      {props.brand[0]}
+    <div className='selected Perfumes' > {
+      
+      <Card.Group>
+        <Card>
+          <Card.Content>
+            <Image src={props.img[0]} />
+            <Card.Header>
+              {props.name[0]}
+            </Card.Header>
+            <Card.Description>
+              brand={props.brand[0]}
+            </Card.Description>
+            <Card.Meta>
+              meta stuff
+            </Card.Meta>
+            <Card.Content extra>
+              <div className='ui buttons'>
+                <Button 
+                  onClick={props.removeOnClick} 
+                  name={props.name[0]}
+                  img={props.img[0]}
+                  brand={props.brand[0]}
+                  
+                  basic color='red'> Remove 
+                </Button>
+              </div>
+            </Card.Content>
+          </Card.Content>
+        </Card>
+        
+        {(props.name.length > 1 ) ? 
+          <Card>
+            <Card.Content>
+              <Image src={props.img[1]} />
+              <Card.Header>
+                {props.name[1]}
+              </Card.Header>
+              <Card.Description>
+               {props.brand[1]}
+              </Card.Description>
+              <Card.Meta>
+                second card meta stuff
+              </Card.Meta>
+              <Card.Content extra>
+                <div className='ui buttons' onClick={props.removeOnClick.bind(this)} >
+                  <Button 
+                    name={props.name[1]}
+                    img={props.img[1]}
+                    brand={props.brand[1]}
+                    basic color='red'> Remove 
+                  </Button>
+                </div>
+              </Card.Content>
+            </Card.Content>
+          </Card>
+        : null }
 
-      <h2> You have selected:  </h2>
-      <div> one thing </div>
+        {(props.name.length > 2 ) ? 
+          <Card>
+            <Card.Content>
+              <Image src={props.img[2]} />
+              <Card.Header>
+                {props.name[2]}
+              </Card.Header>
+              <Card.Description>
+               {props.brand[2]}
+              </Card.Description>
+              <Card.Meta>
+                no. 3 meta meta 
+              </Card.Meta>
+              <Card.Content extra>
+                <div className='ui buttons'  onClick={props.removeOnClick} >
+                  <Button 
+                    name={props.name[2]}
+                    img={props.img[2]}
+                    brand={props.brand[2]}
+                    basic color='red'> Remove 
+                  </Button>
+                </div>
+              </Card.Content>
+            </Card.Content>
+          </Card>
+        : null } 
+
+      </Card.Group>
+
+    }
     </div>
   );
 };
