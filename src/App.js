86e1afc4667e0621ecb234496e2c092f428f60a6
christinechoi <smi77y@gmail.com>
2017-11-16 17:18:22 -0500

    Change SavedPerfumesContainer to RecommendationsContainer and add props to mapStateToProps

--- src/App.js
+++ src/App.js
@@ -2,37 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchPerfume, deletePerfume, getRecommendation } from  './actions/perfumesActions';
+import { Divider, Segment } from 'semantic-ui-react'
 
 import logo from './logo.svg';
 import './App.css';
 import PerfumesListContainer from './containers/PerfumesListContainer';
-import SavedPerfumesContainer from './containers/SavedPerfumesContainer';
+import RecommendationsContainer from './containers/RecommendationsContainer';
 
 class App extends Component {
 
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
         </header>
 
         <PerfumesListContainer  />
 
-        <SavedPerfumesContainer />
+        <RecommendationsContainer />  
+      
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => { 
-  // {debugger};
+  {debugger};
   return { 
     perfumes: state.perfumes.perfumes,
     img: state.perfumes.img,
     name: state.perfumes.name,
-    brand: state.perfumes.brand
+    brand: state.perfumes.brand,
+    id: state.perfumes.id,
+    recommendations: state.perfumes.recommendations,
+    basedOn: state.perfumes.basedOn
    };
 };
 
