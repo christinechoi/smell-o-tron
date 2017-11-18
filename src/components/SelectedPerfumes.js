cde578977823ee8f0c54c21022498bcaccbb0b91
christinechoi <smi77y@gmail.com>
2017-11-18 18:00:47 -0500

    Add button to get recommendations; clean up code

--- src/components/SelectedPerfumes.js
+++ src/components/SelectedPerfumes.js
@@ -1,97 +1,53 @@
 import React from 'react';
-import { Card, Button, Image } from 'semantic-ui-react';
+import { Card, Button, Image, Divider, Sticky } from 'semantic-ui-react';
 
 const SelectedPerfumes = (props) => {
 
-  {debugger};
+
   return (
     <div className='selected Perfumes' > {
-      
-      <Card.Group>
-        <Card>
-          <Card.Content>
-            <Image src={props.img[0]} />
-            <Card.Header>
-              {props.name[0]}
-            </Card.Header>
-            <Card.Description>
-              brand={props.brand[0]}
-            </Card.Description>
-            <Card.Meta>
-              meta stuff
-            </Card.Meta>
-            <Card.Content extra>
-              <div className='ui buttons'>
-                <Button 
-                  onClick={props.removeOnClick} 
-                  name={props.name[0]}
-                  img={props.img[0]}
-                  brand={props.brand[0]}
-                  
-                  basic color='red'> Remove 
-                </Button>
-              </div>
-            </Card.Content>
-          </Card.Content>
-        </Card>
-        
-        {(props.name.length > 1 ) ? 
-          <Card>
-            <Card.Content>
-              <Image src={props.img[1]} />
-              <Card.Header>
-                {props.name[1]}
-              </Card.Header>
-              <Card.Description>
-               {props.brand[1]}
-              </Card.Description>
-              <Card.Meta>
-                second card meta stuff
-              </Card.Meta>
-              <Card.Content extra>
-                <div className='ui buttons' onClick={props.removeOnClick.bind(this)} >
-                  <Button 
-                    name={props.name[1]}
-                    img={props.img[1]}
-                    brand={props.brand[1]}
-                    basic color='red'> Remove 
-                  </Button>
-                </div>
-              </Card.Content>
-            </Card.Content>
-          </Card>
-        : null }
+      <Sticky >
+      <Card.Group className='ui eight column doubling stackable grid container centered'> {
+        props.selectedPerfumes.map((perfume, index) => (  
 
-        {(props.name.length > 2 ) ? 
-          <Card>
+          <Card centered>
             <Card.Content>
-              <Image src={props.img[2]} />
+              <Image src={perfume.pictureURL} size='medium' /> //padding
               <Card.Header>
-                {props.name[2]}
-              </Card.Header>
+                {perfume.name} 
+              </Card.Header> 
               <Card.Description>
-               {props.brand[2]}
+                by {perfume.brand}
               </Card.Description>
               <Card.Meta>
-                no. 3 meta meta 
+                meta stuff
               </Card.Meta>
               <Card.Content extra>
-                <div className='ui buttons'  onClick={props.removeOnClick} >
+                <div className='ui buttons'>
                   <Button 
-                    name={props.name[2]}
-                    img={props.img[2]}
-                    brand={props.brand[2]}
+                    onClick={props.removeOnClick.bind(this, perfume)}
                     basic color='red'> Remove 
-                  </Button>
+                  </Button> //stick to bottom of Card
                 </div>
               </Card.Content>
             </Card.Content>
           </Card>
-        : null } 
+        ))}
 
       </Card.Group>
-
+      </Sticky>
     }
+    <Divider></Divider>
+    
+    <Sticky >
+    <Button 
+      size='big' 
+      color='blue' 
+      onClick={props.handleClick.bind(this, props.selectedPerfumes.map(item => item.id))} > 
+        Get Recommendation! 
+    </Button>
+    </Sticky >
+
     </div>
   );
 };
