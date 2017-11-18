e873d30ff691145561bb81ed8b2b78c0b599aa12
christinechoi <smi77y@gmail.com>
2017-11-18 18:03:47 -0500

    Refactor delete action; filter fetch action to eliminate duplicates

--- src/reducers/perfumesReducer.js
+++ src/reducers/perfumesReducer.js
@@ -1,10 +1,7 @@
 
 const initialState = {
   perfumes: [],
-  img: [],
-  name: [],
-  brand: [],
-  id: [],
+  selectedPerfumes: [],
   recommendations: [],
   basedOn: []
 }
@@ -13,45 +10,40 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_PERFUME':
       {debugger};
-      let uniqueArray = action.payload;
-      
-      return { ...state, perfumes: uniqueArray}
+      let uniqueArray = [];
+
+      action.payload.filter(function(perfume) {
+        return uniqueArray.indexOf(perfume.id) == -1 && uniqueArray.push(perfume.id)
+      });
+
+      let lastArray = [];
+
+      uniqueArray.map(function(perfumeId) {
+        var item = action.payload.find(function(item) {
+            return item.id === perfumeId
+          });
+        lastArray.push(item)
+      });
+
+      return { ...state, perfumes: lastArray}
     case 'ADD_PERFUME':
      
       {debugger};
       return {  
         ...state, 
-        img: state.img.concat(action.payload.src),
-        name: state.name.concat(action.perfume.name), 
-        brand: state.brand.concat(action.perfume.brand),
-        id: state.id.concat(action.perfume.id)
+        selectedPerfumes: state.selectedPerfumes.concat(action.perfume)
       }
 
     case 'DELETE_PERFUME':
-      let ind = state.name.indexOf(action.payload.name); 
+      {debugger};
+      let moddedArray = state.selectedPerfumes.filter(function(perfume) {
+        return perfume.id != action.perfume.id
+      })
 
-      let ary = state.img
-
-      let ex = ary.filter(function (x) {
-        let idx = state.img.indexOf(x);
-        return idx != ind
-      });
-
-      let modName = state.name.filter(function(x) {
-        let idx = state.name.indexOf(x);
-        return idx != ind 
-      });
-
-      let modBrand = state.brand.filter(function(x) {
-        let idx = state.brand.indexOf(x);
-        return idx != ind
-      });
-
+      {debugger};
       return { 
         ...state, 
-        img: ex,
-        name: modName, 
-        brand: modBrand 
+        selectedPerfumes: moddedArray
       };  
     case 'GET_RECOMMENDATION':
       {debugger};
